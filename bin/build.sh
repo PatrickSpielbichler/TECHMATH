c53cf47c8b5557624c2a525ed4df3b97cbf12333
Fabian Achammer <fabian.achammer@gmail.com>
2017-11-14 21:05:33 +0100

    added build script that iterates over exercise folders

---
+++ bin/build.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+echo "Starting LaTeX builds"
+mkdir _build
+
+for semester in ./*/
+do
+    cd semester
+    for subject in ./${semester}/*/
+    do
+        cd subject
+        for exercise in ./${semester}/${subject}/*/
+        do
+            cd exercise
+            filename=${subject,,}-${exercise,,}
+            pdflatex -interaction=nonstopmode -halt-onerror ${filename}.tex
+            cp ${filename}.pdf ../../../_build/${semester}/${subject}
+            cd ..
+        done
+        cd ..
+    done
+    cd ..
+done
+
+echo "Finished LaTeX builds"
