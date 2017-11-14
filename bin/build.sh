4000897babe60c461e3f2c9855f7a4c42ebba24a
Fabian Achammer <fabian.achammer@gmail.com>
2017-11-14 21:09:59 +0100

    fixed bash syntax

--- bin/build.sh
+++ bin/build.sh
@@ -4,13 +4,13 @@ mkdir _build
 
 for semester in ./*/
 do
-    cd semester
+    cd ${semester}
     for subject in ./${semester}/*/
     do
-        cd subject
+        cd ${subject}
         for exercise in ./${semester}/${subject}/*/
         do
-            cd exercise
+            cd ${exercise}
             filename=${subject,,}-${exercise,,}
             pdflatex -interaction=nonstopmode -halt-onerror ${filename}.tex
             cp ${filename}.pdf ../../../_build/${semester}/${subject}
