cf7e4491b18fc5e1eccf264e9bfd67b0ca16f21e
Fabian Achammer <fabian.achammer@gmail.com>
2017-11-14 20:18:15 +0100

    fixed google upload path

--- bin/upload.sh
+++ bin/upload.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 echo "Starting Google Drive upload"
-./bin/gdrive upload --recursive --refresh-token $GDRIVE_REFRESH_TOKEN --parent $GDRIVE_DIR "build_/2017WS/"
+./bin/gdrive upload --recursive --refresh-token $GDRIVE_REFRESH_TOKEN --parent $GDRIVE_DIR "_build/2017WS"
 echo "Finished Google Drive upload"
