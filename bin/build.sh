037112e2c075087578aec91bcb740244b4e672c6
Fabian Achammer <fabian.achammer@gmail.com>
2017-11-14 21:15:40 +0100

    changed to bin/bash and fixed slashes

--- bin/build.sh
+++ bin/build.sh
@@ -1,19 +1,19 @@
-#!/bin/sh
+#!/bin/bash
 echo "Starting LaTeX builds"
 mkdir _build
 
 for semester in ./*/
 do
     cd ${semester}
-    for subject in ./${semester}/*/
+    for subject in ${semester}*/
     do
         cd ${subject}
-        for exercise in ./${semester}/${subject}/*/
+        for exercise in ${semester}${subject}*/
         do
             cd ${exercise}
             filename=${subject,,}-${exercise,,}
             pdflatex -interaction=nonstopmode -halt-onerror ${filename}.tex
-            cp ${filename}.pdf ../../../_build/${semester}/${subject}
+            cp ${filename}.pdf ../../../_build/${semester}${subject}
             cd ..
         done
         cd ..
