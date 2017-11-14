b4428faaf07cefe816a970c89039fea71be075f2
Fabian Achammer <fabian.achammer@gmail.com>
2017-11-14 23:58:58 +0100

    changed upload to sync

--- bin/upload.sh
+++ bin/upload.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Starting Google Drive upload"
-./bin/gdrive upload --recursive --refresh-token $GDRIVE_REFRESH_TOKEN --parent $GDRIVE_DIR "_build/2017WS"
+./bin/gdrive sync upload --refresh-token $GDRIVE_REFRESH_TOKEN _build $GDRIVE_DIR
 echo "Finished Google Drive upload"
