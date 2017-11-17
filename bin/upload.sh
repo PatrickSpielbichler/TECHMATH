6501f1147dbcdd22cdd2bc13962c7e258aa564d1
Fabian Achammer <fabian.achammer@gmail.com>
2017-11-17 23:42:33 +0100

    added multiple branch support

--- bin/upload.sh
+++ bin/upload.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Starting Google Drive upload"
-./bin/gdrive sync upload --keep-local --refresh-token $GDRIVE_REFRESH_TOKEN _build $GDRIVE_DIR
+./bin/gdrive sync upload --keep-local --delete-extraneous --refresh-token $GDRIVE_REFRESH_TOKEN _build $GDRIVE_DIR
 echo "Finished Google Drive upload"
