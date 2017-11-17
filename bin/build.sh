6f1d30aabb1d5a782c351c778072a7034d270d0c
Fabian Achammer <fabian.achammer@gmail.com>
2017-11-17 10:04:10 +0100

    added second pdflatex run to fix references

--- bin/build.sh
+++ bin/build.sh
@@ -26,6 +26,7 @@ do
                 filename=${subject,,}-${exercise,,}
                 if [ -f ${filename}.tex ]; then
                     /tmp/texlive/bin/x86_64-linux/pdflatex -interaction=nonstopmode -halt-on-error ${filename}.tex
+                    /tmp/texlive/bin/x86_64-linux/pdflatex -interaction=nonstopmode -halt-on-error ${filename}.tex
                     mv ${filename}.pdf ../../../../_build/${semester}/${subject}/${filename}.pdf
                 fi
                 cd ../
